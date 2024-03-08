cddc69c8d058a494aeea4ab92e0573fffdefb214
std3 <67806187+standard3@users.noreply.github.com>
2024-03-08 23:43:22 +0100

    fix: performance boost

--- hosts/jupiter/hardware/pm.nix
+++ hosts/jupiter/hardware/pm.nix
@@ -1,13 +1,15 @@
 {
+  services.power-profiles-daemon.enable = false;
+
   services.tlp = {
     enable = true;
     settings = {
       # CPU
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "balanced";
 
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balanced";
 
       CPU_MIN_PERF_ON_AC  = 0;
       CPU_MAX_PERF_ON_AC  = 100;
@@ -15,7 +17,7 @@
       CPU_MAX_PERF_ON_BAT = 20;
 
       # iGPU
-      INTEL_GPU_MIN_FREQ_ON_AC  = 300;
+      INTEL_GPU_MIN_FREQ_ON_AC  = 600;
       INTEL_GPU_MAX_FREQ_ON_AC  = 1500;
       INTEL_GPU_MIN_FREQ_ON_BAT = 300;
       INTEL_GPU_MAX_FREQ_ON_BAT = 1300;
