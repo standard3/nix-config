b27234ed1cd8ca6144a139be06c8349d2b4d7e7a
std3 <67806187+standard3@users.noreply.github.com>
2024-03-09 15:42:22 +0100

    feat: added comment

--- home/applications/dev/rust.nix
+++ home/applications/dev/rust.nix
@@ -2,12 +2,11 @@
 
 {
   home.packages = with pkgs; [
+    # maybe see https://discourse.nixos.org/t/helix-rust-analyzer-failed-to-spawn-one-or-more-proc-macro-servers/35982/2
     # Rust environment
     cargo
     rustc
     rust-analyzer
-    rust-src
-    rustlings
     rustfmt
     clippy
   ];
