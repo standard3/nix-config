62ca7cad159949a91fb31d3be7ab0d4fe04ecade
std3 <67806187+standard3@users.noreply.github.com>
2024-03-12 20:59:53 +0100

    feat: removed useless nixpkgs config

--- hosts/jupiter/home-manager.nix
+++ hosts/jupiter/home-manager.nix
@@ -9,12 +9,6 @@
     overlays = with outputs.overlays; [
       unstable-packages
     ];
-
-    # Nixpkgs instance
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = _: true; # Workaround for https://github.com/nix-community/home-manager/issues/2942
-    };
   };
 
   home-manager = {
@@ -22,7 +16,6 @@
     users = {
       abel = import ../../home/users/abel.nix;
     };
-
     useGlobalPkgs = true;
   };
 }
